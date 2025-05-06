2d181bc7b44900bbdb76718d42e09b33abbea58b
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-06 07:09:44 +0530

    Add DecimalComparator class to implement a method for comparing double values up to three decimal places

---
+++ src/DecimalComparator.java
@@ -0,0 +1,41 @@
+/**
+ * Building A Decimal Comparator To Practice Floating-Point Comparisons In Java
+ * Write a method areEqualByThreeDecimalPlaces with two parameters of type
+ * double.
+ *
+ * The method should return boolean and it needs to return true if two double
+ * numbers are the same up to three decimal places. Otherwise, return false.
+ *
+ *
+ *
+ * EXAMPLES OF INPUT/OUTPUT:
+ *
+ * areEqualByThreeDecimalPlaces(-3.1756, -3.175); → should return true since
+ * numbers are equal up to 3 decimal places.
+ *
+ * areEqualByThreeDecimalPlaces(3.175, 3.176); → should return false since
+ * numbers are not equal up to 3 decimal places
+ *
+ * areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers
+ * are equal up to 3 decimal places.
+ *
+ * areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since
+ * numbers are not equal up to 3 decimal places.
+ */
+
+public class DecimalComparator {
+  public static void main(String[] args) {
+    System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175) == true);
+    System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176) == false);
+    System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0) == true);
+    System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123) == false);
+  }
+
+  public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+    long firstNumber = (long) (num1 * 1000);
+    long secondNumber = (long) (num2 * 1000);
+
+    return firstNumber == secondNumber;
+  }
+
+}
